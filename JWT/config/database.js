2521827ea819d815904b989395a3cf4dd9c98fbd
RahulKatakam <katakam.rahul98@gmail.com>
2022-04-01 14:21:52 +0530

    JWT

---
+++ JWT/config/database.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const MONGO_URI = 'mongodb://localhost:27017/usersdb'
+
+exports.connect = () => {
+  // Connecting to the database
+  mongoose
+    .connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then(() => {
+      console.log("Successfully connected to database");
+    })
+    .catch((error) => {
+      console.log("database connection failed. exiting now...");
+      console.error(error);
+      process.exit(1);
+    });
+};
+
+
+// Mongo DB LocalDB
---
+++ JWT/index.js
@@ -0,0 +1,7 @@
+const http = require("http");
+const app = require("./app");
+const server = http.createServer(app);
+
+server.listen(3005, () => {
+  console.log(`Server running on port 3005`);
+});
